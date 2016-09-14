ca27398764db622d9b6ae6a03b85d1008885d9cd
Masaya_Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-15 01:25:49 +0900

    [add]アンカーが壁に張り付くように実装

---
+++ Project/SmallLegend/Assets/Scripts/Main/Anchor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;using System.Collections;public class Anchor : MonoBehaviour {
+    Rigidbody rigidbody_;
+
+    void Awake() {
+        rigidbody_ = GetComponent<Rigidbody>();
+    }
+
+    // 何かにぶつかったら飛びつける場所か判断してその場所を通知
+    void OnCollisionEnter(Collision other) {
+
+        GameObject parent = FindParent(other.gameObject);
+
+        switch (parent.tag) {
+
+            case "Environment":
+            case "Enemy":
+                Debug.Log("Collide");
+                rigidbody_.velocity = Vector3.zero;
+                break;
+
+        }
+
+
+    }
+
+    // 一番親のオブジェクトのタグを持ってくる
+    GameObject FindParent(GameObject child) {
+
+        if(child.transform.parent == null) {
+            return child;
+        }
+        else {
+            return FindParent(child.transform.parent.gameObject);
+        }
+        
+    }}
