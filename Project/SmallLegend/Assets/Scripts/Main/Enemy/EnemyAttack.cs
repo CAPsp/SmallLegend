e2ce1961a7714c59f4b67ed9ba64540a94bd1976
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-19 17:29:40 +0900

    [add]敵の攻撃とプレイヤーのHPを実装

---
+++ Project/SmallLegend/Assets/Scripts/Main/Enemy/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+	public float boundVelocity_ = 10f;	// 跳ね返る速度
+
+	GameObject player_;
+	PlayerHelth playerHelth_;
+	Rigidbody rigidbody_;
+
+	void Awake(){
+		player_ 		= GameObject.FindGameObjectWithTag ("Player");
+		playerHelth_ 	= player_.GetComponent<PlayerHelth> ();
+		rigidbody_ 		= GetComponent<Rigidbody> ();
+	}
+
+	void OnCollisionEnter(Collision other) {
+
+		if (other.gameObject == player_) {
+			playerHelth_.TakeDamage ();
+
+			// プレイヤーと逆方向に吹っ飛ぶ
+			Vector3 angle = transform.position - other.transform.position;
+			angle.Normalize();
+			rigidbody_.velocity = angle * boundVelocity_;
+
+		}
+
+    }
+
+}
---
+++ Project/SmallLegend/Assets/Scripts/Main/Player/PlayerHelth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHelth : MonoBehaviour {
+
+	public float nonDamageTime_ = 1f;
+
+	int helth_ = 5;
+	float timer_;
+
+	void Awake(){
+		timer_ = nonDamageTime_;
+	}
+
+	void Update(){
+
+		if (timer_ < nonDamageTime_) {
+			timer_ += Time.deltaTime;
+		}
+
+	}
+
+	// 受けるダメージは常に１
+	public void TakeDamage(){
+
+		// 無敵時間中は攻撃を受けない
+		if (timer_ < nonDamageTime_) {
+			return;
+		}
+
+		helth_--;
+
+		Debug.Log (helth_);
+
+	}
+
+}
