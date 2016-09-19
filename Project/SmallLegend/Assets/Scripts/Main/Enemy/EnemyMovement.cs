21a6bad140e117ae7bbdfa73dd33e2783a665213
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-19 16:58:36 +0900

    [add]敵の移動処理を追加

---
+++ Project/SmallLegend/Assets/Scripts/Main/Enemy/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float searchDistance_ = 10f;	// 索敵距離
+
+	Vector3 startPoint_;
+	Transform player_;
+	NavMeshAgent nav_;
+
+	void Awake(){
+		startPoint_ = transform.position;
+		player_ 	= GameObject.FindGameObjectWithTag ("Player").transform;
+		nav_ 		= GetComponent<NavMeshAgent>();
+	}
+
+	void Update(){
+
+		float distance = Vector3.Distance(player_.position, transform.position);
+
+		if (distance <= searchDistance_) {
+			nav_.SetDestination (player_.position);
+		} 
+		else {
+			nav_.SetDestination (startPoint_);
+		}
+
+	}
+
+}
