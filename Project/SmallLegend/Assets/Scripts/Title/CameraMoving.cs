c743c47069d3f0c0e7435ca605a1bf95c2a3b66f
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-22 22:45:23 +0900

    [add]タイトル画面の背景が動く機能を実装(まだ具体的な動き方は作ってない)

--- Project/SmallLegend/Assets/Scripts/Title/CameraMoving.cs
+++ Project/SmallLegend/Assets/Scripts/Title/CameraMoving.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class CameraMoving : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField] float moveSpeed_ 	= 0.5f;
+	[SerializeField] float rotateSpeed_ = 0.5f;
+	[SerializeField] GameObject[] candidatePositions_;
+
+	Transform destination_;
+	int index_;
+
+	void Start(){
+		destination_ = PositionSelect (-1);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
+		transform.position = Vector3.Lerp (transform.position, destination_.position, Time.deltaTime * moveSpeed_);
+		transform.rotation = Quaternion.Lerp (transform.rotation, destination_.rotation, Time.deltaTime * rotateSpeed_);
+
+		// 目的地についたら次の候補を選ぶ
+		if (Vector3.Distance (transform.position, destination_.position) <= 1.0f) {
+			destination_ = PositionSelect (index_);
+		}
+
+	}
+
+	// ランダムに次のカメラの動きの指標となるオブジェクトを候補から選ぶ
+	Transform PositionSelect(int before){
+
+		// 直前のものと同じにならないよう選択
+		do{
+			index_ = Random.Range (0, candidatePositions_.Length);
+		}while(index_ == before);
+
+		Transform[] children = candidatePositions_[index_].transform.GetComponentsInChildren<Transform>();
+		transform.position = children [1].position;
+		transform.rotation = children [1].rotation;
+
+		return children [2];
 	}
 }
