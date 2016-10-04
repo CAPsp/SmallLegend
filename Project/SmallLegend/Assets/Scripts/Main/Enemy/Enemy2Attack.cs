bdbd06bad6c7eb19f76d12cdffa5dfbfcff296d6
Masaya_Kobayashi <kobayashi03@hotmail.co.jp>
2016-10-04 18:51:42 +0900

    [fix]モデルの向きやあたり判定を調整

--- Project/SmallLegend/Assets/Scripts/Main/Enemy/Enemy2Attack.cs
+++ Project/SmallLegend/Assets/Scripts/Main/Enemy/Enemy2Attack.cs
@@ -3,54 +3,64 @@ using System.Collections;
 
 public class Enemy2Attack : MonoBehaviour {
 
-	[SerializeField] float searchDistance_ 	= 15f;
-	[SerializeField] GameObject prefabAmmo_;
-	[SerializeField] float speed_ 			= 20f;
-	[SerializeField] Transform shotPoint_;
-	[SerializeField] float intervalTime_ 	= 1f;
+    [SerializeField] float searchDistance_ = 15f;
+    [SerializeField] GameObject prefabAmmo_;
+    [SerializeField] float speed_ = 20f;
+    [SerializeField] Transform shotPoint_;
+    [SerializeField] float intervalTime_ = 1f;
 
-	GameObject player_;
-	float timer_;
+    GameObject player_;
+    float timer_;
 
-	void Awake(){
-		player_ = GameObject.FindGameObjectWithTag ("Player");
-		timer_ 	= intervalTime_;
-	}
+    void Awake() {
+        player_ = GameObject.FindGameObjectWithTag("Player");
+        timer_ = intervalTime_;
+    }
 
-	void Update(){
+    void Update() {
 
-		if (timer_ < intervalTime_) {
-			timer_ += Time.deltaTime;
-			return;
-		}
+        timer_ += (timer_ < intervalTime_) ? Time.deltaTime : 0f;
 
-		if (DistanceFromPlayer () < searchDistance_) {
-			Shot ();
-		}
+        if (DistanceFromPlayer() < searchDistance_) {
 
-	}
+            Rotation();
 
-	float DistanceFromPlayer(){
-		return Vector3.Distance(transform.position, player_.transform.position);
-	}
+            if (timer_ >= intervalTime_) {
+                Shot();
+            }
+        }
+
+    }
+
+    float DistanceFromPlayer() {
+        return Vector3.Distance(transform.position, player_.transform.position);
+    }
+
+    void Rotation() {
+
+        // プレイヤーの方向を向く(y軸のみ回転)
+        Quaternion targetAngle = Quaternion.FromToRotation(transform.forward, CalcAngleBetweenPlayer());
+        transform.Rotate(new Vector3(0f, targetAngle.eulerAngles.y, 0f));
+
+    }
 
 	void Shot(){
 
-		// プレイヤーとの角度を計算
-		Vector3 angle = player_.transform.position - shotPoint_.position;
-		angle.Normalize ();
-
-
-		// プレイヤーの方向を向く(y軸のみ回転)
-		Quaternion targetAngle = Quaternion.FromToRotation (transform.forward, angle);
-		transform.Rotate( new Vector3(0f, targetAngle.eulerAngles.y, 0f) );
-
 		// 弾を撃ち出す
 		GameObject ammo = Instantiate(prefabAmmo_, shotPoint_.position, Quaternion.identity) as GameObject;
-		ammo.GetComponent<Rigidbody>().velocity = angle * speed_;
+		ammo.GetComponent<Rigidbody>().velocity = CalcAngleBetweenPlayer() * speed_;
 
 		timer_ = 0f;
 
 	}
+
+    // プレイヤーとの角度を計算
+    Vector3 CalcAngleBetweenPlayer() {
+
+        Vector3 angle = player_.transform.position - shotPoint_.position;
+        angle.Normalize();
+
+        return angle;
+    }
 
 }
