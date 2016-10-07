a8bf14ff158458e543eb04069c030b4c5eac9923
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-10-07 12:30:44 +0900

    [add]Show君から渡された素材を反映 [fix]タイトル画面編集中

--- Project/SmallLegend/Assets/Scripts/Main/UI/CurrentWeaponUI.cs
+++ Project/SmallLegend/Assets/Scripts/Main/UI/CurrentWeaponUI.cs
@@ -4,15 +4,25 @@ using UnityEngine.UI;
 
 public class CurrentWeaponUI : MonoBehaviour {
 
+	[SerializeField] Sprite bowSprite_;
+	[SerializeField] Sprite machinGunSprite_;
+
 	Image image_;
 
 	void Awake(){
 		image_ = GetComponent<Image> ();
-		image_.material = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Bow>().gameObject.GetComponent<MeshRenderer>().material;
+		image_.sprite = bowSprite_;
 	}
 
 	public void ChangeImageUI(Material to){
-		image_.material = to;
+
+		if (to == GameObject.FindWithTag ("Player").GetComponentInChildren<Bow> ().GetComponent<MeshRenderer> ().material) {
+			image_.sprite = bowSprite_;
+		}
+		else {
+			image_.sprite = machinGunSprite_;
+		}
+
 	}
 
 }
